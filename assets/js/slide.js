e2779f5f876d16417642c2069fc474c155400c52
Hoàng Nam <nnam44565@gmail.com>
2022-05-20 00:00:54 +0700

    slide

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,26 +1,29 @@
-
-$('.center').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+let menu = ['01', '02', '03']
+let swiper = new Swiper('.swiper-container.two', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+    //change text for each slide
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  paginationClickable: true,
+    effect: 'coverflow',
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: "2" ,
+    coverflow: {
+        rotate: 0,
+        stretch: 100,
+        depth: 150,
+        modifier: 1.5,
+        slideShadows: false,
+    },
+    autoplay: {
+      delay: 2000,
+    },
+});
